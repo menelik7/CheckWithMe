7e2ba14402b2b552a59890f052e58a815f1e0858
Menelik Tefera <menelikworku@gmail.com>
2017-07-07 02:46:36 -0400

    fully functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,9 +10,9 @@
   firebase.initializeApp(config);
 
   var database = firebase.database();
-  database.ref().onDisconnect().set({
-    result:0,
-  });
+  // database.ref().onDisconnect().set({
+  //   result: 0 
+  // });
 
   //Get Elements
   const txtEmail = document.getElementById('email');
@@ -23,6 +23,8 @@
   const wrapper = document.getElementById('wrapper');
   const container = document.getElementById('container');
   const search_again = document.getElementById('search-again');
+  const google_maps = document.getElementById('google-maps');
+  const reset_password = document.getElementById('reset-password');
 
   //Add login event
   btnLogin.addEventListener('click', e => {
@@ -71,45 +73,27 @@
     }
   })
 
-
-  // auth.singInWithEmailAndPassword(email, pass);
-  // auth.createUserWithEmailAndPassword(email, pass);
-
-
-  var login_attempts=3;
-  function check_form() {
-    var name=document.getElementById("name").value;
-    var pass=document.getElementById("pass").value;
-      if(name=="talkerscode" && pass=="talkerscode"){
-        alert("SuccessFully Logged In");
-        document.getElementById("name").value="";
-        document.getElementById("pass").value="";
-      }
-      else{
-        if(login_attempts==0){
-          alert("No Login Attempts Available");
-        }
-        else{
-          login_attempts=login_attempts-1;
-          alert("Login Failed Now Only "+login_attempts+" Login Attempts Available");
-            if(login_attempts==0){
-            document.getElementById("name").disabled=true;
-            document.getElementById("pass").disabled=true;
-            document.getElementById("form1").disabled=true;
-            }
-        }
-      }
-    return false;
-  } 
-
+  reset_password.addEventListener('click', e => {
+    var emailAdd = prompt("Please provide an email address, and check your inbox for instructions");
+    var auth = firebase.auth();
+    var emailAddress = emailAdd;
+    auth.sendPasswordResetEmail(emailAddress).then(function() {
+      // Email sent.
+    }, function(error) {
+      // An error happened.
+    });
+  });
 
 $("#btn").on("click", function(event) {
   event.preventDefault();
+  database.ref().set({
+    result: 0 
+  });
 
 	// Grabbing text the user typed into the search input
 	var input = $("#phone").val().trim();
   $("#phone").val("");
-	var queryURL ="https://proapi.whitepages.com/3.0/phone.json?api_key=dd6cf1d08eeb47038e757269a297655e&phone="+ input;
+	var queryURL ="https://proapi.whitepages.com/3.0/phone.json?api_key=28294d0daa2d455582c10fbf26d651f7&phone="+ input;
 
 	$.ajax({
     url: queryURL,
@@ -122,18 +106,29 @@ $("#btn").on("click", function(event) {
   });
 
   $('#form').hide();
+  var display = $('<div>');
+  display.attr('id', 'display');
+
+  var phoneRef = database.ref('result/phone_number');
+  phoneRef.on('value', function(snapshot){
+    var telnum = snapshot.val();
+    var format = '';
+    char = { 0: '(', 3: ') ', 6: ' - ' };
+      for (var i = 0; i < telnum.length; i++) {
+          format += (char[i] || '') + telnum[i];
+      }
+    $('#check').text("Phone Number: " + format);
+  },function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
 
   var nameRef = database.ref('result/belongs_to/0');
   nameRef.on('value', function(data){
     var name = "<strong>Name</strong>: " + data.val().name;
     var gender = "<strong>Gender</strong>: " + data.val().gender;
     var age = "<strong>Age Range</strong>: " + data.val().age_range;
-
-    var display = $('<div>');
-    display.attr('id', 'display');
-    display.prepend("<strong><span style='color:#2F6FB9'>IDENTITY</span></strong><br>" + name + "<br>" + gender + "<br>" + age);
+    display.prepend("<strong><span style='color:#2F6FB9'>BELONGS TO</span></strong><br>" + name + "<br>" + gender + "<br>" + age + "<br>");
     $('#result').prepend(display);
-    $('#check').html("Results");
   },function(errorObject) {
     console.log("The read failed: " + errorObject.code);
   });
@@ -142,9 +137,7 @@ $("#btn").on("click", function(event) {
   knownAssociatesRef.on('value', function(data){
     var name = "<br><strong><span style='color:#2F6FB9'>KNOWN ASSOCIATES</span></strong><br><strong>Name</strong>: " + data.val().name;
     var relation = "<strong>Relation</strong>: " + data.val().relation;
-    var display = $('<div>');
-    display.attr('id', 'display');
-    display.append(name + "<br>" + relation);
+    display.append(name + "<br>" + relation + "<br>");
     $('#result').append(display);
   },function(errorObject) {
   console.log("The read failed: " + errorObject.code);
@@ -154,9 +147,7 @@ $("#btn").on("click", function(event) {
   knownAssociatesRef.on('value', function(data){
     var name = "<br><strong>Name</strong>: " + data.val().name;
     var relation = "<strong>Relation</strong>: " + data.val().relation;
-    var display = $('<div>');
-    display.attr('id', 'display');
-    display.append(name + "<br>" + relation);
+    display.append(name + "<br>" + relation + "<br>");
     $('#result').append(display);
   },function(errorObject) {
   console.log("The read failed: " + errorObject.code);
@@ -165,9 +156,7 @@ $("#btn").on("click", function(event) {
   var addressRef = database.ref('result/current_addresses/0');
   addressRef.on('value', function(data){
     var address = "<br><strong><span style='color:#2F6FB9'>KNOWN ADDRESSES</span></strong><br><strong>Address 1</strong>: " + data.val().street_line_1 + ", " + data.val().city + " " + data.val().state_code + " " + data.val().postal_code;
-    var display = $('<div>');
-    display.attr('id', 'display');
-    display.append(address);
+    display.append(address + "<br>");
     $('#result').append(display);
   },function(errorObject) {
   console.log("The read failed: " + errorObject.code);
@@ -176,36 +165,59 @@ $("#btn").on("click", function(event) {
   var addressRef = database.ref('result/current_addresses/1');
   addressRef.on('value', function(data){
     var address = "<strong>Address 2</strong>: " + data.val().street_line_1 + ", " + data.val().city + " " + data.val().state_code + " " + data.val().postal_code;
-    var display = $('<div>');
-    display.attr('id', 'display');
-    display.append(address);
+    display.append(address + "<br>");
     $('#result').append(display);
   },function(errorObject) {
   console.log("The read failed: " + errorObject.code);
   });
 
-  $('.btn-default').show()
+  var warningRef = database.ref('result/warnings/0');
+  warningRef.on('value', function(snapshot){
+    var val = snapshot.val();
+    if (val){
+      var invalidInput = snapshot.val();
+      display.html("<strong><span style='color:orange'>" + invalidInput + "</span></strong><br>");
+      $('#check').html("<strong><span style='color:#fff'>WARNING</span></strong><br>");
+      $('#result').html(display);
+      google_maps.classList.add('hide');
+    $('#search-again').html("Try Again?");
+    }
+  },function(errorObject) {
+  console.log("The read failed: " + errorObject.code);
+  });
+
+  google_maps.classList.remove('hide');
   search_again.classList.remove('hide');
 });
 
 function initMap() {
-  var lat_longRef = database.ref('result/current_addresses/0/lat_long');
-  lat_longRef.on('value', function(data){
-    var lati = data.val().latitude;
-    var long = data.val().longitude;
-    var uluru = {lat: lati, lng: long};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 10,
-      center: uluru
+  database.ref('result/current_addresses/1').on('value', function(snapshot){
+    if (snapshot.child('lat_long').exists()) {
+      var lat_longRef = database.ref('result/current_addresses/1/lat_long');
+    }
+    else {
+      var lat_longRef = database.ref('result/current_addresses/0/lat_long');
+    }
+
+    lat_longRef.on('value', function(snapshot){
+      var lati = snapshot.val().latitude;
+      var long = snapshot.val().longitude;
+      var uluru = {lat: lati, lng: long};
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: uluru
+      });
+      var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+      })
     });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    })
-  })
+  });
 }
 
+google_maps.addEventListener('click', e => {
 initMap();
+});
 
 
 
